e2e77fefb1acfc8314d4c8096ee957d20fb107d7
distolma <djmakaron2009@gmail.com>
2018-01-20 13:37:57 +0200

    remove comments

--- rollup.config.ts
+++ rollup.config.ts
@@ -12,22 +12,14 @@ export default {
     { file: pkg.module, format: 'es' },
   ],
   sourcemap: true,
-  // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
   external: [],
   watch: {
     include: 'src/**',
   },
   plugins: [
-    // Compile TypeScript files
     typescript(),
-    // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
     commonjs(),
-    // // Allow node_modules resolution, so you can use 'external' to control
-    // // which external modules to include in the bundle
-    // // https://github.com/rollup/rollup-plugin-node-resolve#usage
     resolve(),
-
-    // Resolve source maps to the original source
     sourceMaps(),
   ],
 }

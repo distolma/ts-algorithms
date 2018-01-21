366536782554efd1336d7dfea3dc43a1cf371575
distolma <djmakaron2009@gmail.com>
2018-01-21 22:19:51 +0200

    add jsdocs to sieveOfEratosthenes

--- src/sieveOfEratosthenes/index.ts
+++ src/sieveOfEratosthenes/index.ts
@@ -1,3 +1,25 @@
+/**
+ * Sieve of Eratosthenes.
+ *
+ * Simple, ancient algorithm for finding all prime numbers up to given limit.
+ *
+ * Returns list of primes up to specified limit.
+ *
+ * For example, for limit 10 it should return following list of primes:
+ * [2, 3, 5, 7].
+ *
+ * @module sieveOfEratosthenes
+ * @param {Number} num - Algorithm will returns list of primes up to
+ * specified limit.
+ * @returns {Array} Will return list with all prime numbers up to provided.
+ * limit.
+ *
+ * @example
+ * var sieveOfEratosthenes =
+ * require('path-to-algorithms/src/sieve-of-eratosthenes').sieveOfEratosthenes;
+ *
+ * console.log(sieveOfEratosthenes(12)); // [2, 3, 5, 7, 11]
+ */
 export function sieveOfEratosthenes(num: number): number[] {
   let primes: boolean[] = [];
   for (let i = 0; i <= num; i++) {

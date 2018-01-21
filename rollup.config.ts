e063f2affe8bb1356eccedc5975183f18d1f9566
distolma <djmakaron2009@gmail.com>
2018-01-21 08:36:45 +0200

    fix rollup config

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,14 +1,15 @@
-import resolve from 'rollup-plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs'
-import sourceMaps from 'rollup-plugin-sourcemaps'
-import typescript from 'rollup-plugin-typescript2'
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import sourceMaps from 'rollup-plugin-sourcemaps';
+import typescript from 'rollup-plugin-typescript2';
+import camelCase from 'lodash/camelCase';
 
 const pkg = require('./package.json')
 
 export default {
   input: `src/index.ts`,
   output: [
-    { file: pkg.main, name: 'index', format: 'umd' },
+    { file: pkg.main, name: camelCase(pkg.name), format: 'umd' },
     { file: pkg.module, format: 'es' },
   ],
   sourcemap: true,

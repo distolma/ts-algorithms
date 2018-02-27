9b183fd0e8938b21d3226340286bc77a8ebfb616
Dmitry Mostovoy <dmitry.mostovoy@idealscorp.com>
2018-02-27 11:13:22 +0200

    replace lodash with lodash-es

--- rollup.config.ts
+++ rollup.config.ts
@@ -2,7 +2,7 @@ import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import sourceMaps from 'rollup-plugin-sourcemaps';
 import typescript from 'rollup-plugin-typescript2';
-import camelCase from 'lodash/camelCase';
+import camelCase from 'lodash-es/camelCase';
 
 const pkg = require('./package.json')
 
